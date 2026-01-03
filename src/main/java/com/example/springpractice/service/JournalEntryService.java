571fd6c11858483967252bf59d6b15be3624783e
Ayush Girulkar <girulkarayush@gmail.com>
2026-01-04 00:05:48 +0530

    Enable MongoDB transactions and update connection URI
    Added transaction management support for MongoDB by enabling @EnableTransactionManagement and defining a MongoTransactionManager bean. Updated JournalEntryService to use @Transactional and improved error handling in saveEntry. Changed MongoDB connection configuration to use a connection URI instead of host and port.

--- src/main/java/com/example/springpractice/service/JournalEntryService.java
+++ src/main/java/com/example/springpractice/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.example.springpractice.reposiratory.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,12 +20,20 @@ public class JournalEntryService
 @Autowired
 private UserService userService;
 
+@Transactional
 public void saveEntry(JournalEntry journalEntry, String userName)
-{   User user =userService.findByUserName(userName);
-    journalEntry.setDate(LocalDateTime.now());
-    JournalEntry saved = journalEntryRepository.save(journalEntry);
-    user.getJournalEntries().add(saved);
-    userService.saveEntry(user);
+{   try {
+                User user =userService.findByUserName(userName);
+                journalEntry.setDate(LocalDateTime.now());
+                JournalEntry saved = journalEntryRepository.save(journalEntry);
+                user.getJournalEntries().add(saved);
+                userService.saveEntry(user);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+            throw new RuntimeException("error occur when try to save entry ",e);
+        }
 }
     public void saveEntry(JournalEntry journalEntry)
     {
